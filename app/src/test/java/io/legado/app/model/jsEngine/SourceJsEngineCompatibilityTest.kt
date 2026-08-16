f11138668fffa5e3c33e47eb60ecb802fed6a428
Jie <1614094+yagay@users.noreply.github.com>
2026-08-16 13:17:21 +0100

    test: run JS compatibility suite on supported Robolectric SDK

--- app/src/test/java/io/legado/app/model/jsEngine/SourceJsEngineCompatibilityTest.kt
+++ app/src/test/java/io/legado/app/model/jsEngine/SourceJsEngineCompatibilityTest.kt
@@ -8,8 +8,10 @@ import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [36])
 class SourceJsEngineCompatibilityTest {
 
     private val sourceKeys = mutableListOf<String>()
